864b559e87dc961bbca8f5c9e99948d8149a36d7
BiaoCai1993 <108630440+BiaoCai1993@users.noreply.github.com>
2024-02-28 18:50:56 -0500

    Update simulation2.R

--- code/simulation2.R
+++ code/simulation2.R
@@ -16,7 +16,7 @@ CTS_proportion=constraint_ols(sig=sig,bulk=bulk)
 lambda_ols <- tune_select(y_all=bulk,W=t(sig),propor_est=t(CTS_proportion),lambda_set=seq(0,0.5,by=0.025),tune_method="sequential")
 sigma_ols0<-sigma_upt0(propor_all=t(CTS_proportion),y_all=bulk,W=t(sig),lambda_all=lambda_ols[[1]])
 
-CTS_proportion_var=propor_cov0(W=t(sig),sigma_all=sigma_ols,propor_all=t(CTS_proportion))
+CTS_proportion_var=propor_cov0(W=t(sig),sigma_all=sigma_ols0,propor_all=t(CTS_proportion))
 dim(CTS_proportion_var) # CTS_proportions_var * samples
 decals_res=list(CTS_proportion=CTS_proportion,CTS_proportion_var=CTS_proportion_var)
 
